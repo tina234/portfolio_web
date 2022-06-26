9f4d0af6e3eafbd9895c3c317f79eadc637ce192
tina234 <martina.dejanovic94@gmail.com>
2022-06-26 20:55:26 +0200

    styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,15 +7,15 @@
 			<div class="header-content">
 				<h3 class="header-title">Passionate <br> frontend developer</h3>
 				<p class="header-desc">Hello, my name is Martina and I’m a passionate frontend developer from Rijeka, Croatia. 
-				I have experience creating websites and small applications using technologies (HTML, CSS, Vue.js , ...).
+				I have experience creating websites and small applications using technologies (HTML, CSS, Vue.js, Git ...).
 				My goal is to improve my current skills day by day and I am looking forward to learning from more experienced developers.
 				</p>
 				<div class="header-btns">
-					<button>Contact me</button>
-					<button>View Resume</button>
+					<a href="mailto:martina.dejanovic94@gmail.com">Contact me</a>
+					<a href="../assets/CV.docx" download>View Resume</a>
 				</div>
 			</div>
-			<div class="bottom-arrow">
+			<div class="bottom-arrow" @click="goToProjects()">
 				<span class="title">View projects</span>
 				<span class="vector">
 					<img src="../assets/arrow_down.svg" alt="">
@@ -39,7 +39,7 @@
 					<img src="../assets/coffe_shop.svg" alt="">
 				</div>
 				<div class="project-content">
-					<h3 class="project-title">Coffe shop</h3>
+					<h3 class="project-title">Coffee shop</h3>
 					<p class="project-desc">Neki opis kratki</p>
 					<a href="https://tina234.github.io/coffee_shop/" target="_blank" class="project-btn">View project</a>
 				</div>
@@ -61,5 +61,14 @@
 
 <script>
 export default {
+	methods: {
+		goToProjects() {
+			const element = document.getElementsByClassName("project");
+			window.scrollTo({
+				top: element[0].offsetTop - '50',
+				behavior: 'smooth'
+			});
+		}
+	}
 }
 </script>
