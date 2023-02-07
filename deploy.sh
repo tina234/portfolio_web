05a83b9a605eae78b893fcdc3d43b70ae871f26b
tina234 <martina.dejanovic94@gmail.com>
2023-02-07 20:52:49 +0100

    deploy sh chanegs

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ cd dist
 
 git init
 git add -A
-git commit -m 'New Deployment'
-git push -f https://github.com/tina234/portfolio_web.git master:gh-pages
+git commit -m 'New Deployment 2023'
+git push -f https://github.com/tina234/portfolio_web.git master:gh-pages2023
 
 cd -
