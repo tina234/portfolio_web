933328763fc657bedd1006acb8b61fae6f38e2d5
tina234 <martina.dejanovic94@gmail.com>
2023-02-20 20:59:12 +0100

    changes at portfolio

---
+++ src/components/header/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="header">
+			<div class="header-img">
+				<img src="../../assets/header_image.svg" alt="">
+			</div>
+			<VueWriter 
+				:array="arr" 
+				:typeSpeed="80" 
+				:eraseSpeed="40" 
+				:delay="1000" 
+				:start="1000"
+				:caret="cursor"
+				/>
+		</div>
+</template>
+
+<script setup>
+const arr = ["Hello!", "My name is Martina.", "I’m a passionate frontend developer."]
+
+/*const goToProjects = () => {
+	const element = document.getElementsByClassName("project");
+	window.scrollTo({
+		top: element[0].offsetTop - '50',
+		behavior: 'smooth'
+	});
+}*/
+</script>
+
+<style lang="scss">
+@import "./header.scss";
+</style>
---
+++ src/components/projects/Projects.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="projects"> 
+			<div class="project">
+				<div class="project-img">
+					<img src="../../assets/blogr.svg" alt="">
+				</div>
+				<div class="project-content">
+					<h3 class="project-title">Blogr</h3>
+					<p class="project-desc">The single-page web site is built in Vue.js framework that offers the user an insight into how the Blogr application works. It demonstrates skills for creating a drop-down menu, simple animations on hover and knowledge of responsiveness.
+					</p>
+					<a href="https://tina234.github.io/blogr/" target="_blank" class="project-btn"><span>View project</span></a>
+				</div>
+			</div>
+			<div class="project">
+				<div class="project-img">
+					<img src="../../assets/coffe_shop.svg" alt="">
+				</div>
+				<div class="project-content">
+					<h3 class="project-title">Coffee shop</h3>
+					<p class="project-desc">Simple web site presenting coffee products made in the Vue.js framework using his libereries like vuex and router. In vuex, I use a couple of modules with JSON data in them. A router is used for multiple views in which I have multiple components.</p>
+					<a href="https://tina234.github.io/coffee_shop/" target="_blank" class="project-btn"><span>View project</span></a>
+				</div>
+			</div>
+			<div class="project">
+				<div class="project-img">
+					<img src="../../assets/github.svg" alt="">
+				</div>
+				<div class="project-content">
+					<h3 class="project-title">Github profile</h3>
+					<p class="project-desc">Here are some more projects on my profile that haven't been published on github pages. Check it out. </p>
+					<a href="https://github.com/tina234" target="_blank" class="project-btn"><span>View profile</span></a>
+				</div>
+			</div>
+		</div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss">
+@import "./projects.scss";
+</style>
